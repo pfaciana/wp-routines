88a08d65fb619ba78bec75f92276a07a35f08aff
Phil Faciana <phil@renderdev.com>
2022-11-14 21:37:06 -0500

    Check to make sure WordPress has loaded

--- load.php
+++ load.php
@@ -1,6 +1,6 @@
 <?php
 
-if ( !defined( 'WP_ROUTINES_PLUGIN_FILE' ) ) {
+if ( !defined( 'WP_ROUTINES_PLUGIN_FILE' ) && isset( $GLOBALS['wp'] ) ) {
 	WP_Routines\Routines::get_instance();
 	define( 'WP_ROUTINES_PLUGIN_DIR', ( __DIR__ ) );
 	define( 'WP_ROUTINES_PLUGIN_FILE', ( __FILE__ ) );
